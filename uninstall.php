eb7ba6704e805ed1bb5498739d1d646bde9b14b0
Verdi-B <verdi.burgers@ictoria.nl>
2024-08-12 10:26:38 +0200

    uninstall update before testing

--- uninstall.php
+++ uninstall.php
@@ -31,4 +31,75 @@ for($i = 0; $i < count($ids); $i++)
 delete_option('allergens_dietary_ictoria_options');
 delete_option('allergens_dietary_ictoria_settings');
 
+//get db object
+global $wpdb;
+
+/**
+ * Array of tables to be deleted during the uninstallation process.
+ *
+ * This array contains the names of database tables that are associated with the allergens and dietary plugin.
+ * These tables will be deleted when the plugin is uninstalled.
+ *
+ * @var array
+ */
+$tables = array(
+	'allergens_dietary_ictoria_attachments',
+	'allergens_dietary_ictoria_allergy',
+	'allergens_dietary_ictoria_allergy_attachment',
+	'allergens_dietary_ictoria_allergy_product'	
+);
+
+/**
+ * Array of foreign keys to be deleted during the uninstallation process.
+ *
+ * This array contains the SQL statements that are used to delete foreign keys from the database tables.
+ */
+$fk_del = array(
+	"ALTER TABLE {$wpdb->prefix}allergens_dietary_ictoria_allergy_product
+	DROP FOREIGN KEY FK_AllergyProduct_WCproduct,
+	DROP FOREIGN KEY FK_AllergyProduct_Allergy",
+	"ALTER TABLE {$wpdb->prefix}allergens_dietary_ictoria_allergy_attachment
+	DROP FOREIGN KEY FK_AllergyAttach_Allergy,
+	DROP FOREIGN KEY FK_AllergyAttach_Attach"
+);
+
+/**
+ * 
+ */
+function delete_fk($fk){
+	global $wpdb;
+	$wpdb->get_results($fk);
+
+	if($wpdb->last_error){
+		$wpdb->flush();
+		return;
+	}
+
+	return delete_fk($fk);
+}
+
+function delete_tables($table){
+	global $wpdb;
+	$query = "DROP TABLE {$table}";
+	$wpdb->get_results($query);
+
+	if($wpdb->last_error){
+		$wpdb->flush();
+		return;
+	}
+
+	return delete_tables($table);
+}
+
+$wpdb->hide_errors();
+foreach($fk_del as $fk){
+	delete_fk($fk);
+}
+
+foreach($tables as $table){
+	$table_name = $wpdb->prefix . $table;
+	delete_tables($table_name);
+}
+
+
 ?>
