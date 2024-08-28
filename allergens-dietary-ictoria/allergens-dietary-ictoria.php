8965e39ca5e0000ce3f15d7c1818bd791554d40f
rworang-ictoria <roy.worang@ictoria.nl>
2024-08-28 15:10:53 +0200

    separated section_main
    - Added an autoloader for the sections and pages directories.
    - Added Poppins font import
    - Added @import in the main css file for the dashboard, might need to move the imports into the dashboard index.php if the css gets too bloated.
    
    The dashboard import in the main plugin file also needs to be moved later on.

--- allergens-dietary-ictoria/allergens-dietary-ictoria.php
+++ allergens-dietary-ictoria/allergens-dietary-ictoria.php
@@ -142,7 +142,7 @@ if(ALLERGENS_DIETARY_ICTORIA_WC_ACTIVE){
 	include_once(ALLERGENS_DIETARY_ICTORIA_DIRNAME.'/php/filter.php');
 
 	// Load dashboard
-	include_once(ALLERGENS_DIETARY_ICTORIA_DIRNAME.'/php/dashboard.php');
+	include_once(ALLERGENS_DIETARY_ICTORIA_DIRNAME.'/php/dashboard/index.php');
 	
 	Allergens_Dietary_Ictoria_Products::instance();
 	Allergens_Dietary_Ictoria_Filter::instance();
