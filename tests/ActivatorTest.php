b4ee145be1051fab7f277b9a1781e214c1159f4b
Verdi-B <verdi.burgers@ictoria.nl>
2024-08-28 16:02:10 +0200

    still need to test the filtertest.php and make the other unit tests

---
+++ tests/ActivatorTest.php
@@ -0,0 +1,36 @@
+<?php 
+
+use PHPUnit\Framework\TestCase;
+
+require_once '/var/www/html/wp-content/plugins/allergens-dietary-ictoria/php/activator.php';
+
+final class ActivatorTest extends TestCase
+{
+    public function test_activate()
+    {
+        $this->assertTrue(class_exists('Allergens_Dietary_Ictoria_Activator'));
+    }
+
+    public function test_initial_counter_value()
+    {
+        $reflector = new ReflectionClass('Allergens_Dietary_Ictoria_Activator');
+        $property = $reflector->getProperty('counter');
+        $property->setAccessible(true);
+        $this->assertEquals(0, $property->getValue());
+    }
+
+
+    public function test_create_tables_method_exists()
+    {
+        $this->assertTrue(method_exists('Allergens_Dietary_Ictoria_Activator', 'create_tables'));
+    }
+
+    public function test_create_tables_is_private()
+    {
+        $reflector = new ReflectionClass('Allergens_Dietary_Ictoria_Activator');
+        $method = $reflector->getMethod('create_tables');
+        $this->assertTrue($method->isPrivate());
+    }
+
+}
+?>
---
+++ tests/FilterTest.php
@@ -0,0 +1,54 @@
+<?php 
+
+use PHPUnit\Framework\TestCase;
+
+require_once '/var/www/html/wp-content/plugins/allergens-dietary-ictoria/php/filter.php';
+
+class FilterTest extends TestCase
+{
+    public function test_instance()
+    {
+        $this->assertTrue(class_exists('Allergens_Dietary_Ictoria_Filter'));
+    }
+
+    public function test_create_filter_method_exists()
+    {
+        $this->assertTrue(method_exists('Allergens_Dietary_Ictoria_Filter', 'create_filter'));
+    }
+
+    public function test_create_filter_is_public()
+    {
+        $reflector = new ReflectionClass('Allergens_Dietary_Ictoria_Filter');
+        $method = $reflector->getMethod('create_filter');
+        $this->assertTrue($method->isPublic());
+    }
+
+    public function test_filter_query_method_exists()
+    {
+        $this->assertTrue(method_exists('Allergens_Dietary_Ictoria_Filter', 'filter_query'));
+    }
+
+    public function test_filter_query_is_public()
+    {
+        $reflector = new ReflectionClass('Allergens_Dietary_Ictoria_Filter');
+        $method = $reflector->getMethod('filter_query');
+        $this->assertTrue($method->isPublic());
+    }
+
+    public function test_instance_is_singleton()
+    {
+        $reflector = new ReflectionClass('Allergens_Dietary_Ictoria_Filter');
+        $property = $reflector->getProperty('_instance');
+        $property->setAccessible(true);
+        $this->assertNull($property->getValue());
+    }
+    
+    public function test_construct_is_private()
+    {
+        $reflector = new ReflectionClass('Allergens_Dietary_Ictoria_Filter');
+        $method = $reflector->getMethod('__construct');
+        $this->assertTrue($method->isPrivate());
+    }
+}
+
+?>
