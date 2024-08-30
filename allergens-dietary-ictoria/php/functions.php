d26676625defd052e18c82d0248bd7475ce14b3f
jimvogelzangs <91766@roc-teraa.nl>
2024-08-30 16:24:35 +0200

    translation fix

--- allergens-dietary-ictoria/php/functions.php
+++ allergens-dietary-ictoria/php/functions.php
@@ -9,17 +9,17 @@ class Allergens_Dietary_Ictoria_Functions{
 	
 	//function to get a translation of all text contained within __() functions throughout the plugin IF the .mo and .po files for the local/server language are available
 	public static function load_textdomain(){
-		load_plugin_textdomain(__('allergens-dietary-ictoria', 'allergens-dietary-ictoria'), false, basename(ALLERGENS_DIETARY_ICTORIA_FILE).'/l10n'); 
+		load_plugin_textdomain(' allergens-dietary-ictoria', false, basename(ALLERGENS_DIETARY_ICTORIA_FILE).'/l10n'); 
 	}
 	
 	//Get all relevant plugin settings from the WP options table
 	public static function get_settings(){
-		return get_option(__('allergens_dietary_ictoria_settings','allergens-dietary-ictoria'));
+		return get_option('allergens_dietary_ictoria_settings');
 	}
 	
 	//Get all allergens and dietary options added by this plugin from the WP options table
 	public static function get_options(){
-		return get_option(__('allergens_dietary_ictoria_options','allergens-dietary-ictoria'));
+		return get_option('allergens_dietary_ictoria_options');
 	}
 	
 	// //adds the external css file(s) to the current WP execution
