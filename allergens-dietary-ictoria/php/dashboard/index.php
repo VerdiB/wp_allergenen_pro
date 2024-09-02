fc2418dbf5487bda56dc51a498ced8374648ab16
rworang-ictoria <roy.worang@ictoria.nl>
2024-09-02 17:09:08 +0200

    Trying to make the extended classes work
    I think there is an issue where things aren't loaded in the right order maybe. And/or it might also be because of the add_actions being in the base class. All the hooks get loaded, but there might be something wrong in the callbacks as well.

--- allergens-dietary-ictoria/php/dashboard/index.php
+++ allergens-dietary-ictoria/php/dashboard/index.php
@@ -6,12 +6,6 @@ if (!defined('ABSPATH')) {
 
 define('ADI_DASHBOARD_DIR', __DIR__);
 
-// Autoload classes
-spl_autoload_register(['Allergens_Dietary_Ictoria_Dashboard', 'autoload']);
-
-// Initialize the main dashboard class
-Allergens_Dietary_Ictoria_Dashboard::instance();
-
 class Allergens_Dietary_Ictoria_Dashboard
 {
     private static $_instance = null;
@@ -27,73 +21,29 @@ class Allergens_Dietary_Ictoria_Dashboard
     private function __construct()
     {
         // Register actions for the admin menu
-        add_action('admin_menu', [$this, 'dashboard_page']);
-        add_action('admin_enqueue_scripts', [__CLASS__, 'adi_dashboard_style']);
-        add_action('admin_enqueue_scripts', [__CLASS__, 'adi_dashboard_script']);
-
-        // Initialize all necessary components
-        $this->initialize_components();
+        add_action('admin_menu', [$this, 'initialize_components']);
     }
 
-    public static function adi_dashboard_style()
+    public function initialize_components()
     {
-        wp_enqueue_style('adi-dashboard-css', plugins_url('assets/dashboard/css/adi-dashboard.css', ALLERGENS_DIETARY_ICTORIA_FILE));
-    }
-    public static function adi_dashboard_script()
-    {
-        wp_enqueue_script(
-            'adi-dashboard-js',
-            plugins_url('assets/dashboard/js/adi-dashboard.js', ALLERGENS_DIETARY_ICTORIA_FILE),
-            ['jquery'],
-            false,
-            true
-        );
-    }
-
-    // Initialize all dashboard components (pages, sections, etc.)
-    private function initialize_components()
-    {
-        // Initialize the main page and any other components
+        // ADI_Dashboard_Main_Page::instance();
         ADI_Dashboard_Main_Page::instance();
-        // You can add more initializations for other sections here if needed
-        ADI_Dashboard_Main_Section::instance();
-    }
-
-    public function dashboard_page()
-    {
-        add_menu_page(
-            'Ictoria Plugin Dashboard',
-            'Ictoria Plugins',
-            'manage_options',
-            'allergens-dietary-ictoria',
-            [$this, 'load_dashboard_page'],
-            'dashicons-admin-settings',
-            56
-        );
-    }
-
-    public function load_dashboard_page()
-    {
-        $dashboard_page = ADI_Dashboard_Main_Page::instance();
-        $dashboard_page->render_page();
     }
 
     public static function autoload($class_name)
     {
         // Base directory for the namespace prefix
-        $base_dir = ADI_DASHBOARD_DIR . '/';
-
         // Array of directories to search for classes
         $directories = [
-            $base_dir . 'sections/',
-            $base_dir . 'pages/',
+            ADI_DASHBOARD_DIR . '/' . 'sections/',
+            ADI_DASHBOARD_DIR . '/' . 'pages/',
         ];
 
         $file_name = str_replace('_', '-', strtolower($class_name)) . '.php';
-
         if (strpos($class_name, 'ADI_') === 0) {
             foreach ($directories as $directory) {
                 $file = $directory . $file_name;
+                // print_r($file . '<br>');
                 if (file_exists($file)) {
                     require_once $file;
                     return;
@@ -102,3 +52,109 @@ class Allergens_Dietary_Ictoria_Dashboard
         }
     }
 }
+
+// Autoload classes
+spl_autoload_register(['Allergens_Dietary_Ictoria_Dashboard', 'autoload']);
+
+// Initialize the main dashboard class
+Allergens_Dietary_Ictoria_Dashboard::instance();
+
+// define('ADI_DASHBOARD_DIR', __DIR__);
+
+// // Autoload classes
+// spl_autoload_register(['Allergens_Dietary_Ictoria_Dashboard', 'autoload']);
+
+// Initialize the main dashboard class
+// Allergens_Dietary_Ictoria_Dashboard::instance();
+
+// class Allergens_Dietary_Ictoria_Dashboard
+// {
+//     private static $_instance = null;
+
+//     public static function instance()
+//     {
+//         if (is_null(self::$_instance)) {
+//             self::$_instance = new self();
+//         }
+//         return self::$_instance;
+//     }
+
+//     private function __construct()
+//     {
+//         // Register actions for the admin menu
+//         add_action('admin_menu', [$this, 'dashboard_page']);
+//         add_action('admin_enqueue_scripts', [__CLASS__, 'adi_dashboard_style']);
+//         add_action('admin_enqueue_scripts', [__CLASS__, 'adi_dashboard_script']);
+
+//         // Initialize all necessary components
+//         $this->initialize_components();
+//     }
+
+//     public static function adi_dashboard_style()
+//     {
+//         wp_enqueue_style('adi-dashboard-css', plugins_url('assets/dashboard/css/adi-dashboard.css', ALLERGENS_DIETARY_ICTORIA_FILE));
+//     }
+//     public static function adi_dashboard_script()
+//     {
+//         wp_enqueue_script(
+//             'adi-dashboard-js',
+//             plugins_url('assets/dashboard/js/adi-dashboard.js', ALLERGENS_DIETARY_ICTORIA_FILE),
+//             ['jquery'],
+//             false,
+//             true
+//         );
+//     }
+
+//     // Initialize all dashboard components (pages, sections, etc.)
+//     private function initialize_components()
+//     {
+//         // Initialize the main page and any other components
+//         ADI_Dashboard_Main_Page::instance();
+//         // You can add more initializations for other sections here if needed
+//         $section = new ADI_Dashboard_Main_Section();
+//         // ADI_Dashboard_Main_Section::instance();
+//     }
+
+//     public function dashboard_page()
+//     {
+//         add_menu_page(
+//             'Ictoria Plugin Dashboard',
+//             'Ictoria Plugins',
+//             'manage_options',
+//             'allergens-dietary-ictoria',
+//             [$this, 'load_dashboard_page'],
+//             'dashicons-admin-settings',
+//             56
+//         );
+//     }
+
+//     public function load_dashboard_page()
+//     {
+//         $dashboard_page = ADI_Dashboard_Main_Page::instance();
+//         $dashboard_page->render_page();
+//     }
+
+//     public static function autoload($class_name)
+//     {
+//         // Base directory for the namespace prefix
+//         $base_dir = ADI_DASHBOARD_DIR . '/';
+
+//         // Array of directories to search for classes
+//         $directories = [
+//             $base_dir . 'sections/',
+//             $base_dir . 'pages/',
+//         ];
+
+//         $file_name = str_replace('_', '-', strtolower($class_name)) . '.php';
+
+//         if (strpos($class_name, 'ADI_') === 0) {
+//             foreach ($directories as $directory) {
+//                 $file = $directory . $file_name;
+//                 if (file_exists($file)) {
+//                     require_once $file;
+//                     return;
+//                 }
+//             }
+//         }
+//     }
+// }
--- allergens-dietary-ictoria/php/dashboard/sections/adi-dashboard-main-section.php
+++ allergens-dietary-ictoria/php/dashboard/sections/adi-dashboard-main-section.php
@@ -4,7 +4,7 @@ if (!defined('ABSPATH')) {
     exit;
 }
 
-class ADI_Dashboard_Main_Section
+class ADI_Dashboard_Main_Section extends ADI_Base_Dashboard_Section
 {
     private static $_instance = null;
 
@@ -16,129 +16,164 @@ class ADI_Dashboard_Main_Section
         return self::$_instance;
     }
 
-    private function __construct()
+    public function __construct()
     {
-        // Register section in the settings
-        add_action('admin_init', [$this, 'main_section_init']);
-    }
+        parent::__construct('adi_main_section', __('Main section title', 'allergens-dietary-ictoria'), 'allergens-dietary-ictoria');
 
-    public function main_section_init()
-    {
-        add_settings_section(
-            'adi-main-section',
-            __('These are some plugins', 'allergens_dietary_ictoria'),
-            [$this, 'section_main_callback'],
-            'allergens_dietary_ictoria'
-        );
-
-        // add_settings_field(
-        //     'adi-field-pill',
-        //     __('Pill', 'allergens_dietary_ictoria'),
-        //     [$this, 'field_pill_callback'],
-        //     'allergens_dietary_ictoria',
-        //     'adi-main-section',
-        //     [
-        //         'label_for' => 'adi-field-pill',
-        //         'class' => 'adi-field-row',
-        //         'adi_custom_data' => 'custom',
-        //     ]
-        // );
-    }
-
-    public function section_main_callback($args)
-    {
-        echo $this->get_section_main_html($args);
+        // Add specific fields for this section
+        $this->add_field('adi_field_pill', __('Pill', 'allergens-dietary-ictoria'), [$this, 'field_pill_callback']);
     }
 
     public function field_pill_callback($args)
     {
-        echo $this->get_field_pill_html($args);
-    }
-
-    // Generates HTML for the section main callback
-    private function get_section_main_html($args)
-    {
-        // $id = esc_attr($args['id']);
-        // $content = esc_html__('Follow the white rabbit.', 'allergens_dietary_ictoria');
-
-        // return "<p id=\"$id\">$content</p>";
-
-        $html = <<<HTML
-        <div class="adi-dashboard-section">
-            <p>Temporibus, praesentium impedit! Praesentium harum reprehenderit in eaque accusantium facilis ratione.</p>
-        </div>
-
-        <div class="adi-hero">
-            <div class="adi-hero-menu">
-                <span class="adi-hero-menu-item">Hero slide 1</span>
-                <span class="adi-hero-menu-item">Hero slide 2</span>
-                <span class="adi-hero-menu-item">Hero slide 3</span>
-            </div>
-
-            <div class="adi-hero-wrapper">
-                <div class="adi-hero-container">
-                    <div class="adi-hero-content-left">
-                        <h2>Allergens & Dietary Plugin</h2>
-                        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, praesentium impedit! Praesentium harum reprehenderit in eaque accusantium facilis ratione est necessitatibus sequi doloremque quis unde, odit, recusandae itaque laudantium vero?</p>
-                    </div>
-
-                    <div class="adi-hero-content-right">
-
-                        <div class="adi-hero-review">
-                            <h3>Review 1</h3>
-                            <div>
-                                <span>John Doe</span>
-                            </div>
-                            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
-                        </div>
-
-                        <div class="adi-hero-review">
-                            <h3>Review 2</h3>
-                            <div>
-                                <span>Jane Smith</span>
-                            </div>
-                            <p>Quos animi, tempora commodi alias fugiat harum quisquam maiores quibusdam illum aliquam quo.</p>
-                        </div>
-
-                        <div class="adi-hero-review">
-                            <h3>Review 3</h3>
-                            <div>
-                                <span>Alice Johnson</span>
-                            </div>
-                            <p>Sequi laudantium aspernatur veritatis iste nemo temporibus beatae corporis.</p>
-                        </div>
-
-                    </div>
-                </div>
-            </div>
-        </div>
-        HTML;
-
-        return $html;
-    }
-
-    // Generates HTML for the field pill callback
-    private function get_field_pill_html($args)
-    {
         $options = get_option('allergens_dietary_ictoria_dashboard_options');
-        $label_for = esc_attr($args['label_for']);
-        $custom_data = esc_attr($args['adi_custom_data']);
-        $red_selected = selected($options[$args['label_for']], 'red', false);
-        $blue_selected = selected($options[$args['label_for']], 'blue', false);
-
-        $html = <<<HTML
-        <select id="$label_for" data-custom="$custom_data" name="allergens_dietary_ictoria_dashboard_options[$label_for]">
-            <option value="red" $red_selected>Red Pill</option>
-            <option value="blue" $blue_selected>Blue Pill</option>
-        </select>
-        <p class="description">
-            You take the blue pill and the story ends. You wake in your bed and you believe whatever you want to believe.
-        </p>
-        <p class="description">
-            You take the red pill and you stay in Wonderland and I show you how deep the rabbit-hole goes.
-        </p>
-        HTML;
-
-        return $html;
+        $html = '<select id="' . esc_attr($args['label_for']) . '"
+                    data-custom="' . esc_attr($args['adi_custom_data']) . '"
+                    name="allergens_dietary_ictoria_dashboard_options[' . esc_attr($args['label_for']) . ']">
+                    <option value="red" ' . selected($options[$args['label_for']], 'red', false) . '>' . esc_html__('red pill', 'allergens-dietary-ictoria') . '</option>
+                    <option value="blue" ' . selected($options[$args['label_for']], 'blue', false) . '>' . esc_html__('blue pill', 'allergens-dietary-ictoria') . '</option>
+                </select>';
+        $html .= '<p class="description">' . esc_html__('You take the blue pill and the story ends. You wake in your bed and you believe whatever you want to believe.', 'allergens-dietary-ictoria') . '</p>';
+        $html .= '<p class="description">' . esc_html__('You take the red pill and you stay in Wonderland and I show you how deep the rabbit-hole goes.', 'allergens-dietary-ictoria') . '</p>';
+        echo $html;
     }
 }
+
+// class ADI_Dashboard_Main_Section
+// {
+//     private static $_instance = null;
+
+//     public static function instance()
+//     {
+//         if (is_null(self::$_instance)) {
+//             self::$_instance = new self();
+//         }
+//         return self::$_instance;
+//     }
+
+//     private function __construct()
+//     {
+//         // Register section in the settings
+//         add_action('admin_init', [$this, 'main_section_init']);
+//     }
+
+//     public function main_section_init()
+//     {
+//         add_settings_section(
+//             'adi-main-section',
+//             __('These are some plugins', 'allergens_dietary_ictoria'),
+//             [$this, 'section_main_callback'],
+//             'allergens_dietary_ictoria'
+//         );
+
+//         // add_settings_field(
+//         //     'adi-field-pill',
+//         //     __('Pill', 'allergens_dietary_ictoria'),
+//         //     [$this, 'field_pill_callback'],
+//         //     'allergens_dietary_ictoria',
+//         //     'adi-main-section',
+//         //     [
+//         //         'label_for' => 'adi-field-pill',
+//         //         'class' => 'adi-field-row',
+//         //         'adi_custom_data' => 'custom',
+//         //     ]
+//         // );
+//     }
+
+//     public function section_main_callback($args)
+//     {
+//         echo $this->get_section_main_html($args);
+//     }
+
+//     public function field_pill_callback($args)
+//     {
+//         echo $this->get_field_pill_html($args);
+//     }
+
+//     // Generates HTML for the section main callback
+//     private function get_section_main_html($args)
+//     {
+//         // $id = esc_attr($args['id']);
+//         // $content = esc_html__('Follow the white rabbit.', 'allergens_dietary_ictoria');
+
+//         // return "<p id=\"$id\">$content</p>";
+
+//         $html = <<<HTML
+//         <div class="adi-dashboard-section">
+//             <p>Temporibus, praesentium impedit! Praesentium harum reprehenderit in eaque accusantium facilis ratione.</p>
+//         </div>
+
+//         <div class="adi-hero">
+//             <div class="adi-hero-menu">
+//                 <span class="adi-hero-menu-item">Hero slide 1</span>
+//                 <span class="adi-hero-menu-item">Hero slide 2</span>
+//                 <span class="adi-hero-menu-item">Hero slide 3</span>
+//             </div>
+
+//             <div class="adi-hero-wrapper">
+//                 <div class="adi-hero-container">
+//                     <div class="adi-hero-content-left">
+//                         <h2>Allergens & Dietary Plugin</h2>
+//                         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Temporibus, praesentium impedit! Praesentium harum reprehenderit in eaque accusantium facilis ratione est necessitatibus sequi doloremque quis unde, odit, recusandae itaque laudantium vero?</p>
+//                     </div>
+
+//                     <div class="adi-hero-content-right">
+
+//                         <div class="adi-hero-review">
+//                             <h3>Review 1</h3>
+//                             <div>
+//                                 <span>John Doe</span>
+//                             </div>
+//                             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+//                         </div>
+
+//                         <div class="adi-hero-review">
+//                             <h3>Review 2</h3>
+//                             <div>
+//                                 <span>Jane Smith</span>
+//                             </div>
+//                             <p>Quos animi, tempora commodi alias fugiat harum quisquam maiores quibusdam illum aliquam quo.</p>
+//                         </div>
+
+//                         <div class="adi-hero-review">
+//                             <h3>Review 3</h3>
+//                             <div>
+//                                 <span>Alice Johnson</span>
+//                             </div>
+//                             <p>Sequi laudantium aspernatur veritatis iste nemo temporibus beatae corporis.</p>
+//                         </div>
+
+//                     </div>
+//                 </div>
+//             </div>
+//         </div>
+//         HTML;
+
+//         return $html;
+//     }
+
+//     // Generates HTML for the field pill callback
+//     private function get_field_pill_html($args)
+//     {
+//         $options = get_option('allergens_dietary_ictoria_dashboard_options');
+//         $label_for = esc_attr($args['label_for']);
+//         $custom_data = esc_attr($args['adi_custom_data']);
+//         $red_selected = selected($options[$args['label_for']], 'red', false);
+//         $blue_selected = selected($options[$args['label_for']], 'blue', false);
+
+//         $html = <<<HTML
+//         <select id="$label_for" data-custom="$custom_data" name="allergens_dietary_ictoria_dashboard_options[$label_for]">
+//             <option value="red" $red_selected>Red Pill</option>
+//             <option value="blue" $blue_selected>Blue Pill</option>
+//         </select>
+//         <p class="description">
+//             You take the blue pill and the story ends. You wake in your bed and you believe whatever you want to believe.
+//         </p>
+//         <p class="description">
+//             You take the red pill and you stay in Wonderland and I show you how deep the rabbit-hole goes.
+//         </p>
+//         HTML;
+
+//         return $html;
+//     }
+// }
