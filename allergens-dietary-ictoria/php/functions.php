0dac04b3401115fbf43ea32ed4bd19c981be93ac
jimvogelzangs <jim.vogelzangs@ictoria.nl>
2024-09-04 11:58:48 +0200

    translation-refix

--- allergens-dietary-ictoria/php/functions.php
+++ allergens-dietary-ictoria/php/functions.php
@@ -24,12 +24,12 @@ class Allergens_Dietary_Ictoria_Functions{
 	
 	//Get all relevant plugin settings from the WP options table
 	public static function get_settings(){
-		return get_option(__('allergens_dietary_ictoria_settings','allergens-dietary-ictoria'));
+		return get_option('allergens_dietary_ictoria_settings');
 	}
 	
 	//Get all allergens and dietary options added by this plugin from the WP options table
 	public static function get_options(){
-		return get_option(__('allergens_dietary_ictoria_options','allergens-dietary-ictoria'));
+		return get_option('allergens_dietary_ictoria_options');
 	}
 	
 	// //adds the external css file(s) to the current WP execution
