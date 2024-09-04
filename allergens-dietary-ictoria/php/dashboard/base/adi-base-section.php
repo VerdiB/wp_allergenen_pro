c81be490f7c6a3d250e285b1c06b62b108012492
rworang-ictoria <roy.worang@ictoria.nl>
2024-09-04 21:21:17 +0200

    Got it working with sub-menus
    It's now easier to add sub-menus.
    
    Still need to fix the options not working, I think I need to add back register_setting() somewhere.
    Also need to cleanup and make things more coherent.
    
    After the cleanup it should be good to go.

---
+++ allergens-dietary-ictoria/php/dashboard/base/adi-base-section.php
@@ -0,0 +1,54 @@
+<?php
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+abstract class ADI_Base_Section
+{
+    protected $section_id;
+    protected $section_title;
+    protected $fields = [];
+
+    public function __construct($section_id, $section_title)
+    {
+        $this->section_id = $section_id;
+        $this->section_title = $section_title;
+    }
+
+    public function register_section($menu_slug)
+    {
+        add_settings_section(
+            $this->section_id,
+            $this->section_title,
+            [$this, 'section_callback'],
+            $menu_slug
+        );
+
+        foreach ($this->fields as $field) {
+            add_settings_field(
+                $field['id'],
+                $field['title'],
+                $field['callback'],
+                $menu_slug,
+                $this->section_id,
+                $field['args']
+            );
+        }
+    }
+
+    public function add_field($field_id, $field_title, $callback, $args = [])
+    {
+        $this->fields[] = [
+            'id' => $field_id,
+            'title' => $field_title,
+            'callback' => $callback,
+            'args' => $args,
+        ];
+    }
+
+    public function section_callback()
+    {
+        echo '<p>Section description here.</p>';
+    }
+}
--- allergens-dietary-ictoria/php/dashboard/index.php
+++ allergens-dietary-ictoria/php/dashboard/index.php
@@ -20,33 +20,23 @@ class Allergens_Dietary_Ictoria_Dashboard
 
     private function __construct()
     {
-        // Register actions for the admin menu
-        // add_action('admin_menu', [$this, 'initialize_components']);
-        add_action('admin_menu', [ADI_Dashboard_Main_Page::instance(), 'add_menu_page']);
-        add_action('admin_init', [ADI_Dashboard_Main_Page::instance(), 'admin_init_hooks']);
-        // add_action did both admin_menu & admin_init inside the wrapping add_action -> admin_menu, might have been the problem
-    }
-
-    public function initialize_components()
-    {
-        // ADI_Dashboard_Main_Page::instance();
-        ADI_Dashboard_Main_Page::instance();
+        // Initialize the dashboard
+        // new ADI_Dashboard_Main_Page();
     }
 
     public static function autoload($class_name)
     {
-        // Base directory for the namespace prefix
         // Array of directories to search for classes
         $directories = [
-            ADI_DASHBOARD_DIR . '/' . 'sections/',
+            ADI_DASHBOARD_DIR . '/' . 'base/',
             ADI_DASHBOARD_DIR . '/' . 'pages/',
+            ADI_DASHBOARD_DIR . '/' . 'sections/',
         ];
 
         $file_name = str_replace('_', '-', strtolower($class_name)) . '.php';
         if (strpos($class_name, 'ADI_') === 0) {
             foreach ($directories as $directory) {
                 $file = $directory . $file_name;
-                // print_r($file . '<br>');
                 if (file_exists($file)) {
                     require_once $file;
                     return;
@@ -61,3 +51,6 @@ spl_autoload_register(['Allergens_Dietary_Ictoria_Dashboard', 'autoload']);
 
 // Initialize the main dashboard class
 Allergens_Dietary_Ictoria_Dashboard::instance();
+
+ADI_Dashboard_Main_Page::instance();
+ADI_Dashboard_Settings_Page::instance();
