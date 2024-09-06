2568c6cc331a4602f62447c77beda7d1a9ce42c1
rworang-ictoria <roy.worang@ictoria.nl>
2024-09-06 12:09:04 +0200

    Load in the base css/js files
    Added enqueue for css/js in index.php, the base assets can just be loaded via there
    
    The separate loading of css/js for each page/section can be added later on.

--- allergens-dietary-ictoria/php/dashboard/index.php
+++ allergens-dietary-ictoria/php/dashboard/index.php
@@ -20,7 +20,25 @@ class Allergens_Dietary_Ictoria_Dashboard
 
     private function __construct()
     {
+        add_action('admin_enqueue_scripts', [__CLASS__, 'adi_dashboard_style']);
+        add_action('admin_enqueue_scripts', [__CLASS__, 'adi_dashboard_script']);
         // Other things can be initialized here if needed
+    }
+
+    public static function adi_dashboard_style()
+    {
+        wp_enqueue_style('adi-dashboard-css', plugins_url('assets/css/adi-dashboard.css', ALLERGENS_DIETARY_ICTORIA_FILE));
+    }
+
+    public static function adi_dashboard_script()
+    {
+        wp_enqueue_script(
+            'adi-dashboard-js',
+            plugins_url('assets/js/adi-dashboard.js', ALLERGENS_DIETARY_ICTORIA_FILE),
+            ['jquery'],
+            false,
+            true
+        );
     }
 
     public static function autoload($class_name)
