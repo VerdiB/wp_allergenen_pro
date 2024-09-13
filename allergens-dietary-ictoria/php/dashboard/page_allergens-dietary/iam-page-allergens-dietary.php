735b0cdf7eeab42c6e5a89a2d8716a2c45672a24
rworang-ictoria <roy.worang@ictoria.nl>
2024-09-13 11:03:46 +0200

    Made dashboard with link to allergens submenu
    FR14 - "Er is een plugin dashboard waar men als hoofdpagina de Ictoria plugin suite ziet waar alle plugins van Ictoria te zien is. "

---
+++ allergens-dietary-ictoria/php/dashboard/page_allergens-dietary/iam-page-allergens-dietary.php
@@ -0,0 +1,41 @@
+<?php
+// Exit if accessed directly
+if (!defined('ABSPATH')) {
+    exit;
+}
+
+class IAM_Page_Allergens_Dietary extends IAM_Base_Page
+{
+    private static $page_sections = [
+    ];
+
+    public function __construct()
+    {
+        parent::__construct(
+            'Allergens & Dietary Plugin',
+            'Allergens & Dietary',
+            'manage_options',
+            'iam-allergens-dietary',
+            [$this, 'render_page'],
+            '',
+            null,
+        );
+
+        foreach (self::$page_sections as $section_class) {
+            $class_name = __CLASS__ . '_Section_' . $section_class;
+            if (class_exists($class_name)) {
+                $this->add_section(new $class_name);
+            }
+        }
+    }
+
+    protected function is_top_level()
+    {
+        return false;
+    }
+
+    protected function get_parent_slug()
+    {
+        return 'iam-dashboard';
+    }
+}
