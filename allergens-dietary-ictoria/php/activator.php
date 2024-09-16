57cc2bdd3514fc507bc61d00dd8f305de26750f6
Verdi-B <verdi.burgers@ictoria.nl>
2024-09-16 10:55:16 +0200

    Revert "Merge pull request #12 from Ictoria-BV/wp-dashboard"
    This reverts commit 7ebfc327b43484e63ea0f9f188b284d36fb9cef0, reversing
    changes made to e82251ce022e7b44bfdac5170985d83dd9a2eb4a.

--- allergens-dietary-ictoria/php/activator.php
+++ allergens-dietary-ictoria/php/activator.php
@@ -1,66 +1,65 @@
 <?php
 
-if (!defined('ABSPATH')) {
-    exit;
+if ( ! defined( 'ABSPATH' ) ) {
+	exit;
 }
 
-class Allergens_Dietary_Ictoria_Activator
-{
-    private static $counter = 0;
 
-    public static function activate()
-    {
-        if (self::$counter === 0) {
-            self::create_tables();
-            // self::add_fk_tables();
-            ++self::$counter;
-        }
-        if (self::$counter > 0) {
-            return;
-        }
-    }
+class Allergens_Dietary_Ictoria_Activator {
+	private static $counter = 0;
 
-    private static function create_tables()
-    {
-        global $wpdb;
-        require_once ABSPATH . 'wp-admin/includes/upgrade.php';
+	public static function activate() {
+		if ( self::$counter === 0 ) {
+			self::create_tables();
+			// self::add_fk_tables();
+			++self::$counter;
+		}
+		if ( self::$counter > 0 ) {
+			return;
+		}
+	}
 
-        $sql_attachments = $wpdb->query(
-            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}allergens_dietary_ictoria_attachments(
-						attachment_name VARCHAR(255) NOT NULL PRIMARY KEY,
-						attachment_path VARCHAR(255))"
-        );
+	private static function create_tables() {
+		global $wpdb;
+		require_once ABSPATH . 'wp-admin/includes/upgrade.php';
 
-        $sql_allergy = $wpdb->query(
-            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}allergens_dietary_ictoria_allergy(
-						allergy_name VARCHAR(50) NOT NULL PRIMARY KEY,
-						allergy_description VARCHAR(255))"
-        );
+		$sql_attachments = $wpdb->query(
+			"CREATE TABLE IF NOT EXISTS {$wpdb->prefix}allergens_dietary_ictoria_attachments(
+        attachment_name VARCHAR(255) NOT NULL PRIMARY KEY,
+        attachment_path VARCHAR(255))"
+		);
 
-        $sql_allergy_attachment = $wpdb->query(
-            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}allergens_dietary_ictoria_allergy_attachment(
-						allergy_name VARCHAR(50) NOT NULL,
-						attachment_name VARCHAR(255) NOT NULL,
-						PRIMARY KEY (allergy_name, attachment_name),
-						CONSTRAINT FK_AllergyAttch_Allergy
-						FOREIGN KEY (allergy_name) REFERENCES {$wpdb->prefix}allergens_dietary_ictoria_allergy(allergy_name),
-						CONSTRAINT FK_AllergyAttch_Attch
-						FOREIGN KEY (attachment_name) REFERENCES {$wpdb->prefix}allergens_dietary_ictoria_attachments(attachment_name))"
-        );
+		$sql_allergy = $wpdb->query(
+			"CREATE TABLE IF NOT EXISTS {$wpdb->prefix}allergens_dietary_ictoria_allergy(
+        allergy_name VARCHAR(50) NOT NULL PRIMARY KEY,
+        allergy_description VARCHAR(255))"
+		);
 
-        $sql_allergy_product = $wpdb->query(
-            "CREATE TABLE IF NOT EXISTS {$wpdb->prefix}allergens_dietary_ictoria_allergy_product(
-						product_id BIGINT NOT NULL,
-						allergy_name VARCHAR(50) NOT NULL,
-						PRIMARY KEY (product_id, allergy_name),
-						CONSTRAINT FK_AllergyProduct_WCproduct
-						FOREIGN KEY (product_id) REFERENCES {$wpdb->prefix}wc_product_meta_lookup(product_id),
-						CONSTRAINT FK_AllergyProduct_Allergy
-						FOREIGN KEY (allergy_name) REFERENCES {$wpdb->prefix}allergens_dietary_ictoria_allergy(allergy_name))"
-        );
-        dbDelta($sql_attachments);
-        dbDelta($sql_allergy);
-        dbDelta($sql_allergy_attachment);
-        dbDelta($sql_allergy_product);
-    }
+		$sql_allergy_attachment = $wpdb->query(
+			"CREATE TABLE IF NOT EXISTS {$wpdb->prefix}allergens_dietary_ictoria_allergy_attachment(
+        allergy_name VARCHAR(50) NOT NULL,
+        attachment_name VARCHAR(255) NOT NULL,
+        PRIMARY KEY (allergy_name, attachment_name),
+        CONSTRAINT FK_AllergyAttch_Allergy
+        FOREIGN KEY (allergy_name) REFERENCES {$wpdb->prefix}allergens_dietary_ictoria_allergy(allergy_name),
+        CONSTRAINT FK_AllergyAttch_Attch
+        FOREIGN KEY (attachment_name) REFERENCES {$wpdb->prefix}allergens_dietary_ictoria_attachments(attachment_name))
+        "
+		);
+
+		$sql_allergy_product = $wpdb->query(
+			"CREATE TABLE IF NOT EXISTS {$wpdb->prefix}allergens_dietary_ictoria_allergy_product(
+        product_id BIGINT NOT NULL,
+        allergy_name VARCHAR(50) NOT NULL,
+        PRIMARY KEY (product_id, allergy_name),
+        CONSTRAINT FK_AllergyProduct_WCproduct
+        FOREIGN KEY (product_id) REFERENCES {$wpdb->prefix}wc_product_meta_lookup(product_id),
+        CONSTRAINT FK_AllergyProduct_Allergy
+        FOREIGN KEY (allergy_name) REFERENCES {$wpdb->prefix}allergens_dietary_ictoria_allergy(allergy_name))"
+		);
+		dbDelta( $sql_attachments );
+		dbDelta( $sql_allergy );
+		dbDelta( $sql_allergy_attachment );
+		dbDelta( $sql_allergy_product );
+	}
 }
--- allergens-dietary-ictoria/php/functions.php
+++ allergens-dietary-ictoria/php/functions.php
@@ -309,29 +309,29 @@ class Allergens_Dietary_Ictoria_Functions {
 			),
 		);
 
-		$attributes_allergen =
-		add_query_arg(
-			array(
-				'key' => array(
-					'category'      => __( 'allergen', 'allergens-dietary-ictoria' ),
-					'title'         => __( 'key', 'allergens-dietary-ictoria' ),
-					'status'        => __( 'active', 'allergens-dietary-ictoria' ),
-					'filter-action' => __( 'exclude', 'allegens-dietary-ictoria' ),
-					'filter-extra'  => $no,
-					'icon'          => __( 'url1', 'allegens-dietary-ictoria' ), // icon upload option in WP-dashboard
-					ALLERGENS_DIETARY_ICTORIA_DIRNAME,
-				),
-			)
-		);
+        $attributes_allergen =
+            add_query_arg(
+            array(
+                'key' => array(
+                    'category' => __('allergen', 'allergens-dietary-ictoria'),
+                    'title' => __('key', 'allergens-dietary-ictoria'),
+                    'status' => __('active', 'allergens-dietary-ictoria'),
+                    'filter-action' => __('exclude', 'allegens-dietary-ictoria'),
+                    'filter-extra' => $no,
+                    'icon' => __('url1', 'allegens-dietary-ictoria'), // icon upload option in WP-dashboard
+                    ALLERGENS_DIETARY_ICTORIA_DIRNAME,
+                ),
+            )
+        );
 
-		// add allergen button in WordPress
-		submit_button(
-			__( 'add allergen', 'allergens-dietary-ictoria' ),
-			'primary', // CSS class for the button
-			true,  // Wrap the button in a <p> tag
-			$attributes_allergen // Additional attributes like 'id'
-		);
+        // add allergen button in WordPress
+        submit_button(
+            __('add allergen', 'allergens-dietary-ictoria'),
+            'primary', // CSS class for the button
+            true, // Wrap the button in a <p> tag
+            $attributes_allergen // Additional attributes like 'id'
+        );
 
-				return $options;
-	}
+        return $options;
+    }
 }
