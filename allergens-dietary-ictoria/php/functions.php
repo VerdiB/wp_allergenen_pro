b174293436cc0c8f4d9965a430b23e2d268540b4
MarcelB21 <marcelbrzezinski@msn.com>
2024-09-20 14:27:36 +0200

    Autoload-tier-1
    Autoload (PHP) not working on my pc!

--- allergens-dietary-ictoria/php/functions.php
+++ allergens-dietary-ictoria/php/functions.php
@@ -4,6 +4,8 @@ if ( ! defined( 'ABSPATH' ) ) {
 	exit;
 }
 
+// class that contains functions that are used by multiple classes/files or do not belong in another class
+
 /**
  * @author V.B.
  * @covers Allergens_Dietary_Ictoria_Functions
@@ -15,14 +17,14 @@ if ( ! defined( 'ABSPATH' ) ) {
 
 class Allergens_Dietary_Ictoria_Functions {
 
-	// function to get a translation of all text contained within __() functions throughout the plugin IF the .mo and .po files for the local/server language are available
-	public static function load_textdomain() {
-		load_plugin_textdomain( __( 'allergens-dietary-ictoria', 'allergens-dietary-ictoria' ), false, basename( ALLERGENS_DIETARY_ICTORIA_FILE ) . '/l10n' );
+	//function to get a translation of all text contained within __() functions throughout the plugin IF the .mo and .po files for the local/server language are available
+	public static function load_textdomain(){
+		load_plugin_textdomain(' allergens-dietary-ictoria', false, basename(ALLERGENS_DIETARY_ICTORIA_FILE).'/l10n'); 
 	}
-
-	// Get all relevant plugin settings from the WP options table
-	public static function get_settings() {
-		return get_option( __( 'allergens_dietary_ictoria_settings', 'allergens-dietary-ictoria' ) );
+	
+	//Get all relevant plugin settings from the WP options table
+	public static function get_settings(){
+		return get_option('allergens_dietary_ictoria_settings');
 	}
 	
 	//Get all allergens and dietary options added by this plugin from the WP options table
@@ -314,3 +316,7 @@ class Allergens_Dietary_Ictoria_Functions {
         return $options;
     }
 }
+
+
+
+?>
