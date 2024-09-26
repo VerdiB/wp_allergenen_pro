98f6b3757caa0c2a25b045aa268c37cb296e16d4
TimKleuver <tim.kleuver@ictoria.nl>
2024-09-26 14:45:08 +0200

    info added

--- allergens-dietary-ictoria/php/MyPluginAddMenu.php
+++ allergens-dietary-ictoria/php/MyPluginAddMenu.php
@@ -14,6 +14,7 @@ class MyPluginAddMenu {
 	 * @staticvar   array   $instance
 	 * @return      The one true instance
 	 */
+	
 	public static function instance() {
 		if ( is_null( self::$instance ) ) {
 			self::$instance = new MyPluginAddMenu();
--- allergens-dietary-ictoria/php/forms/allergen_info.php
+++ allergens-dietary-ictoria/php/forms/allergen_info.php
@@ -21,11 +21,44 @@ class Allergens_Dietary_Ictoria_Info {
     public function showInfo() {
         //flexbox voor tabs
         $html = '<div id="info_grid" class="nav-tab-wrapper">
-        <p class="nav-tab">Change allergens</p>
-        <p class="nav-tab">Change allergens</p>
-        <p class="nav-tab">Change allergens</p>
-        <p class="nav-tab">Change allergens</p>
-        <p class="nav-tab">Change allergens</p>
+        <div><h1 class="premium">PREMIUM  [Requires licence]</h1>
+        <ol>
+            <li class="contains">
+                Updating allergies [PREMIUM]
+            </li>
+            <li class="contains">
+                Changing allergy themes [PREMIUM]
+            </li>
+            <li class="contains">
+                Deleting allergies [PREMIUM]
+            </li>
+             <li class="contains">
+                Adding allergies [PREMIUM]
+            </li>
+            <li class="contains">
+                Custom look on product [PREMIUM]
+            </li>
+            <li class="contains">
+                Custom look in store [PREMIUM]
+            </li>
+        </ol>
+        </div>
+        <div><h1 class="free">FREE VERSION  [Standard]</h1>
+        <ol>
+            <li class="contains">
+                Connecting allergies to products [FREE]
+            </li>
+            <li class="contains">
+                Wordpress theme fiendly styles [FREE]
+            </li>
+            <li class="contains">
+                An allergen overview [FREE]
+            </li>
+            <li class="contains">
+                Turning the the use of allergies on/off [FREE]
+            </li>
+            </ol>
+        </div>
         </div>
         <br> <br>';
         echo $html;
