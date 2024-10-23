d95336a5595e7124b25cabf2e1d2361d7277e5cd
TimKleuver <tim.kleuver@ictoria.nl>
2024-10-23 15:59:37 +0200

    requested changes

--- allergens-dietary-ictoria/php/DB/allergen.php
+++ allergens-dietary-ictoria/php/DB/allergen.php
@@ -66,7 +66,7 @@ class Allergens_Dietary_Ictoria_Allergen_Queries {
 		$table_name = $wpdb->prefix . 'allergens_dietary_ictoria_allergy';
 
 		$sql = $wpdb->prepare(
-			"SELECT * FROM $table_name WHERE allergy_name = %s",
+			"SELECT allergy_name FROM $table_name WHERE allergy_name = %s",
 			$allergenName
 		);
 
@@ -153,8 +153,9 @@ class Allergens_Dietary_Ictoria_Allergen_Queries {
 
 	foreach($allergens_result as $key => $value){
 		$sql = $wpdb->prepare(
-			"SELECT * FROM $table_allergens WHERE allergy_name = '" . $value['title'] . "'"
-		);
+			"SELECT allergy_name FROM $table_allergens WHERE allergy_name = %s"
+			,$value['title']);
+			
 			$exists = $wpdb->get_var( $sql );
 		if ($exists == 0){
 			$isallergy = 0;
