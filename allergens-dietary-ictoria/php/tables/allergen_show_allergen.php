c18a10af207b323ed87d3a358f75996ea7d3e805
TimKleuver <tim.kleuver@ictoria.nl>
2024-10-28 14:04:14 +0100

    fixes

--- allergens-dietary-ictoria/php/tables/allergen_show_allergen.php
+++ allergens-dietary-ictoria/php/tables/allergen_show_allergen.php
@@ -352,7 +352,7 @@ class Allergens_Dietary_Ictoria_Show_Allergens extends WP_List_Table
             $table = new Allergens_Dietary_Ictoria_Show_Allergens();
             $table->prepare_items();
             $self = htmlspecialchars($_SERVER["PHP_SELF"]);
-            echo '<form action="#" method="POST"';
+            echo '<form action="#" method="POST" enctype="multipart/form-data"';
             echo "<table class='wp-list-table widefat fixed striped table-view-list pages'>";
                 $table->display();
             echo "</table>";
