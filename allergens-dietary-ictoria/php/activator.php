8d04f7b1c1cda7e2e531c3049a680c61c58d15b9
TimKleuver <tim.kleuver@ictoria.nl>
2024-10-29 11:13:43 +0100

    foutje.. heb ik weggehaald

--- allergens-dietary-ictoria/php/activator.php
+++ allergens-dietary-ictoria/php/activator.php
@@ -442,15 +442,6 @@ class Allergens_Dietary_Ictoria_Activator
 
 		$page = isset($_GET['page']) ? sanitize_text_field(wp_unslash($_GET['page'])) : '';
 		$exists = $wpdb->get_var($sql);
-		$pageboolean = false;
-
-		if (isset($_GET['page'])){
-			if (in_array($page, $allowed_pages, true)) {
-				$pageboolean = true;
-			}else{
-				$pageboolean = false;
-			}
-		}
 
 		if ($exists > 0) {
 			// Record exists!
