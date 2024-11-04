551e5fdb05b7b54e59eee1ca286550ee23f771ed
TimKleuver <tim.kleuver@ictoria.nl>
2024-11-04 12:52:28 +0100

    changed to update

--- allergens-dietary-ictoria/php/MyPluginAddMenu.php
+++ allergens-dietary-ictoria/php/MyPluginAddMenu.php
@@ -144,7 +144,7 @@ class MyPluginAddMenu
 			require_once ALLERGENS_DIETARY_ICTORIA_DIRNAME . '/php/tabs/allergen_tabs.php';
 		}
 		Allergens_Dietary_Ictoria_Tabs::getInstance()->showtabs();
-		Allergens_Dietary_Ictoria_Form::setFormType(FormType::ALLERGENS);
+		Allergens_Dietary_Ictoria_Form::setFormType(FormType::UPDATE);
 		Allergens_Dietary_Ictoria_Form::getInstance()->showForm();
 	}
 
