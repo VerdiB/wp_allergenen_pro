02a864cb47470bf321aa7023179c816f712093f9
Verdi-B <verdi.burgers@ictoria.nl>
2024-11-05 09:00:44 +0100

    added comments on save_product_options

--- allergens-dietary-ictoria/php/woocommerce/product_settings.php
+++ allergens-dietary-ictoria/php/woocommerce/product_settings.php
@@ -95,6 +95,16 @@ class Allergens_Dietary_Ictoria_Product_Settings {
 	}
 
 	// function that stores all selected options in the productdata of the currently selected product
+
+	/**
+	 * @param int $post_id
+	 * @author V.B.
+	 * @important This method is not yet completed the function on deleting and adding allergens is still bugged
+	 * @brief This method saves or deletes the selected allergens and dietary restrictions to the product. depending on the (de-)selected options 
+	 * @return void
+	 * @since 1.0.0
+	 * @date 5-11-2024
+	 */
 	public function save_product_options( $post_id ) {
 		$allergensSelected = array();
 		unset($this->_attachedAllergens);
@@ -111,13 +121,19 @@ class Allergens_Dietary_Ictoria_Product_Settings {
 		if ( count($allergensSelected) === count($this->_attachedAllergens) &&
 		array_diff($allergensSelected, $this->_attachedAllergens) )
 		{
-			foreach ($allergensSelected as $allergen) {
+			//test the toDelete array
+			$toDelete = array_diff($this->_attachedAllergens, $allergensSelected);
+			foreach ($allergensSelected as $index => $allergen) {
 				$db = Allergens_Dietary_Ictoria_Allergy_Product_Queries::getInstance();
-				if ( count( $db->getAllergyProduct( $post_id, $allergen ) ) === 0 ) {
+				if ( count( $db->getAllergyProduct( $post_id, $allergen ) ) === 0  ) {
 					$db->addAllergyProduct($post_id, $allergen) ;
 				}
-				else {
-					$db->deleteAllergyProduct($post_id, $allergen);
+				if ( !empty( $toDelete ) ) {
+					try{
+						$db->deleteAllergyProduct($post_id, $allergen);
+					}catch(Exception $e){
+						unset($db);
+					}
 				}
 				unset($db);
 			}
