428e2f2e7179f7936aca1266c077000b9d117dd1
Maik Hendriks <Maik.Hendriks@ictoria.nl>
2024-11-05 15:09:21 +0100

    Update loadstyle function

--- allergens-dietary-ictoria/allergens-dietary-ictoria.php
+++ allergens-dietary-ictoria/allergens-dietary-ictoria.php
@@ -163,6 +163,7 @@ if (ALLERGENS_DIETARY_ICTORIA_WC_ACTIVE) {
 
 	Allergens_Dietary_Ictoria_Products::instance();
 	Allergens_Dietary_Ictoria_Filter::instance();
+	Allergens_Dietary_Ictoria_Activator::load_style();
 	include_once ALLERGENS_DIETARY_ICTORIA_DIRNAME . '/php/Allergens_Dietary_Ictoria_Plugin_Menu.php';
 	Allergens_Dietary_Ictoria_Plugin_Menu::instance();
 } else {
