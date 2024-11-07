e3906e1fa192611ebd49d45d27f8054a019c9063
Maik Hendriks <Maik.Hendriks@ictoria.nl>
2024-11-07 10:43:43 +0100

    style fix

--- allergens-dietary-ictoria/php/forms/allergen_update_allergen.php
+++ allergens-dietary-ictoria/php/forms/allergen_update_allergen.php
@@ -111,12 +111,12 @@ class Allergens_Dietary_Ictoria_Update_Allergen_Form implements I_Allergens_Diet
         foreach ($data as $icon) {
             // check if file is an image and if it is not, skip it
             if (false === in_array($icon['type'], $this->MIME_TYPES)) {
-                echo '<p style="color: red;>' . __('The new file: ' . $icon['name'] . ' is not a valid image.  Supported image types are: ' . implode(', ', $this->MIME_NAMES) . '.', 'allergens-dietary-ictoria') . '</p>';
+                echo '<p style="color: red;">' . __('The new file: ' . $icon['name'] . ' is not a valid image.  Supported image types are: ' . implode(', ', $this->MIME_NAMES) . '.', 'allergens-dietary-ictoria') . '</p>';
                 continue;
             }
             // check if file is in database already and if it is, skip it
             if (true === Allergens_Dietary_Ictoria_Attachment_Queries::getInstance()->checkAttachmentExists($icon['name'])) {
-                echo '<p style="color: red;>' . __('The new file: ' . $icon['name'] . ' already exists.', 'allergens-dietary-ictoria') . '</p>';
+                echo '<p style="color: red;">' . __('The new file: ' . $icon['name'] . ' already exists.', 'allergens-dietary-ictoria') . '</p>';
                 continue;
             }
 
