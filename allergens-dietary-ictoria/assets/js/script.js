9d6494ae28aafd40ff78eef96c3e54f761341e7b
ArvindictoriaNL <arvind.manodat@ictoria.nl>
2024-11-08 15:32:56 +0100

    Achtergrond kleur veranderd
    De achtergrond kleur van de meldingen is veranderd naar grijs en alleen aan de linkerkant van de melding zie je een gekleurd streepje.

--- allergens-dietary-ictoria/assets/js/script.js
+++ allergens-dietary-ictoria/assets/js/script.js
@@ -1,18 +1,32 @@
+
 var actionSchedulerError = document.querySelectorAll(".notice-error");
 var actionSchedulerWarning = document.querySelectorAll(".notice-warning");
 var actionSchedulerSucces = document.querySelectorAll(".notice-success");
 
+
 actionSchedulerError.forEach(error => {
-    error.style.backgroundColor = "red";
+    error.style.backgroundColor = "grey"; 
+    error.style.setProperty("background-color", "white", "important"); 
+    error.style.borderLeft = "5px solid red"; 
+    error.style.setProperty("border-left", "5px solid red", "important"); 
+    error.style.paddingLeft = "10px"; 
+    error.style.color = "black"; 
 });
 
 actionSchedulerWarning.forEach(warning => {
-    warning.style.backgroundColor = "orange";
+    warning.style.setProperty("background-color", "white", "important"); 
+    warning.style.setProperty("border-left", "5px solid orange", "important"); 
+    warning.style.paddingLeft = "10px";
+    warning.style.color = "black";
 });
 
 actionSchedulerSucces.forEach(succes => {
-    succes.style.backgroundColor = "green";
+    succes.style.setProperty("background-color", "white", "important"); 
+    succes.style.setProperty("border-left", "5px solid green", "important"); 
+    succes.style.paddingLeft = "10px";
+    succes.style.color = "black";
 });
+
 
 
 var button = document.getElementById("dropdown-ictoria");
