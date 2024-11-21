c888c10cd0e18467d7bc369fbae1bf2b01d13f00
TimKleuver <tim.kleuver@ictoria.nl>
2024-11-21 14:31:30 +0100

    changes

--- allergens-dietary-ictoria/php/forms/allergen_add_allergen.php
+++ allergens-dietary-ictoria/php/forms/allergen_add_allergen.php
@@ -97,16 +97,18 @@ class Allergens_Dietary_Ictoria_Allergen_Form implements I_Allergens_Dietary_Ict
 			$html .= '</select>';
 			$html .= '</div>';
 			$html .= '<label for="allergen_description">' . __('Allergen description', 'allergens-dietary-ictoria') . '</label>';
-			$html .= '<input type="text" class="update_" name="allergen_description" id="allergen_description" value="' . ((!empty($this->_allergen)) ? $this->_allergen['allergy_description'] : '') . '" disabled/>';
-			$html .= '<div class="item" style="display: flex; align-items: center; gap: 15px;">';
+			$html .= '<textarea class="update_" name="allergen_description" id="allergen_description" style="width: 300px; min-height: 100px; resize: none;" maxlength="255" disabled>'. ((!empty($this->_allergen)) ? $this->_allergen['allergy_description'] : '') . '</textarea><br><br>';
+			$html .= '</div><input disabled type="submit" class="update_ button button-primary save" name="submit" class="button button-primary" value="' . __('Update', 'allergens-dietary-ictoria') . '"/><br><br></fieldset>';
+			$html .= '<fieldset class="inline-edit-col-right drag-drop-buttons"><div class="item">';
 			$html .= '<figure style="text-align: center;"><img class="update_ allergen_icon_img" style="max-height: 40px; max-width: 40px;" disabled id="allergen_icon_img" src="' . ((!empty($this->_allergen)) ? $this->_allergen['attachment_path'] : "") . '" alt="' . ((!empty($this->_allergen)) ? $this->_allergen['attachment_name'] : "") . '">';
 			$html .= '<figcaption style="font-size: 10px; color: gray;">The width and height of an icon are max 40px by 40px.</figcaption>';
-			$html .= '</figure>';
-			$html .= '<label class="label-quick-edit">';
+			$html .= '<br><label class="label-quick-edit wp-core-ui button">';
 			$html .= '<input type="file" class="update_ allergen_icon_file_input" accept="image/png, image/jpeg, image/jpg, image/webp, image/svg+xml" name="allergen_icon" id="allergen_icon_file_input" disabled>';
 			$html .= '<input type="hidden" class="update_" name="allergen_icon_hidden" value="' . ((!empty($this->_allergen)) ? $this->_allergen['attachment_name'] : "") . '" disabled>';
 			$html .= '<span>Set image</span>';
-			$html .= '</label></div><br></fieldset>';
+			$html .= '</label>';
+			$html .= '</figure>';
+			$html .= '</div><br></fieldset>';
 			$html .= '</div></fieldset">';
 		} else {
 			$html = '<fieldset>';
