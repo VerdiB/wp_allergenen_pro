0660e05b8fb2119701a1699ffc923fe4501a2527
TimKleuver <tim.kleuver@ictoria.nl>
2024-11-21 15:41:58 +0100

    edited the conform

--- allergens-dietary-ictoria/php/forms/allergen_add_allergen.php
+++ allergens-dietary-ictoria/php/forms/allergen_add_allergen.php
@@ -110,7 +110,10 @@ class Allergens_Dietary_Ictoria_Allergen_Form implements I_Allergens_Dietary_Ict
 			$html .= '</label></div><br></fieldset>';
 			$html .= '</div></fieldset">';
 		} else {
-			$html = '<fieldset>';
+			$html = '<fieldset id="the-list" class="inline-edit-product.quick-edit-row">';
+			$html .= '<div class="inline-edit-wrapper" aria-labelledby="quick-edit-legend">';
+			$html .= '<fieldset class="inline-edit-col-left "><div class="inline-edit-row">';
+			$html .= '<legend class="inline-edit-legend">' . __("Add allergen", "allergens-dietary-ictoria") . '</legend>';
 			$html .= '<label for="allergen_name">' . __('Allergen name', 'allergens-dietary-ictoria') . '</label><br>';
 			$html .= '<input type="text" name="allergen_name" id="allergen_name" value="' . ((!empty($this->_allergen)) ? $this->_allergen['allergy_name'] : '') . '" required/><br><br>';
 			$html .= '<label for="type">' . __('Type', 'allergens-dietary-ictoria') . '</label> <br/>';
@@ -119,14 +122,15 @@ class Allergens_Dietary_Ictoria_Allergen_Form implements I_Allergens_Dietary_Ict
 			$html .= '</select> <br><br>';
 			$html .= '<label for="allergen_description">' . __('Allergen description', 'allergens-dietary-ictoria') . '</label><br>';
 			$html .= '<input type="text" name="allergen_description" id="allergen_description" value="' . ((!empty($this->_allergen)) ? $this->_allergen['allergy_description'] : '') . '"/><br><br>';
-			$html .= '<div class="item">';
+			$html .= '<div class="item"></fieldset><fieldset class="inline-edit-col-right drag-drop-buttons"><div class="item">';
 			$html .= '<img class="update_ allergen_icon_img" id="allergen_icon_img"  style="height: 75px;" src="' . get_home_url() . '/wp-content/plugins/allergens-dietary-ictoria/assets/icons/no_icon_selected.png" alt="no_icon_selected.png"><br>';
 			$html .= '<label class="label-quick-edit wp-core-ui button">';
 			$html .= '<input type="file" class="update_ allergen_icon_file_input" accept="image/png, image/jpeg, image/jpg, image/webp, image/svg+xml" name="allergen_icon" id="allergen_icon_file_input">';
 			$html .= '<input type="hidden" class="update_" name="allergen_icon_hidden" value="' . ((!empty($this->_allergen)) ? $this->_allergen['attachment_name'] : "") . '">';
 			$html .= '<span>Set image</span>';
-			$html .= '</label></div><br>';
+			$html .= '</label></div></fieldset><br>';
 			$html .= '<input type="submit" name="submit" class="button button-primary" value="' . __('Add allergen', 'allergens-dietary-ictoria') . '"/>';
+			$html .= '</div>';
 			$html .= '</fieldset>';
 		}
 
