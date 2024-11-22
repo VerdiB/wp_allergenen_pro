7d07dc359a4f6bd9853be9cc391321f6a43637fb
Verdi-B <verdi.burgers@ictoria.nl>
2024-11-22 13:45:48 +0100

    re-added the nav bar-Swork

--- allergens-dietary-ictoria/php/Allergens_Dietary_Ictoria_Plugin_Menu.php
+++ allergens-dietary-ictoria/php/Allergens_Dietary_Ictoria_Plugin_Menu.php
@@ -125,7 +125,7 @@ class Allergens_Dietary_Ictoria_Plugin_Menu
 			require_once ALLERGENS_DIETARY_ICTORIA_DIRNAME . '/php/forms/allergen_form.php';
 			require_once ALLERGENS_DIETARY_ICTORIA_DIRNAME . '/php/tabs/allergen_tabs.php';
 		}
-		// Allergens_Dietary_Ictoria_Tabs::getInstance()->showtabs();
+		Allergens_Dietary_Ictoria_Tabs::getInstance()->showtabs();
 		Allergens_Dietary_Ictoria_Form::setFormType(FormType::ALLERGENS);
 		Allergens_Dietary_Ictoria_Form::getInstance()->showForm();
 	}
