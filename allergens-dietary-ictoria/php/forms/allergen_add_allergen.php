12db91345d4daa8fc83de7a059e37cb24cb5f5f6
Verdi-B <verdi.burgers@ictoria.nl>
2024-11-25 11:01:34 +0100

    Edited set image

--- allergens-dietary-ictoria/php/forms/allergen_add_allergen.php
+++ allergens-dietary-ictoria/php/forms/allergen_add_allergen.php
@@ -104,7 +104,7 @@ class Allergens_Dietary_Ictoria_Allergen_Form implements I_Allergens_Dietary_Ict
 			$html .= '<label class="label-quick-edit wp-core-ui button">';
 			$html .= '<input type="file" class="update_ allergen_icon_file_input" accept="image/png, image/jpeg, image/jpg, image/webp, image/svg+xml" name="allergen_icon" id="allergen_icon_file_input" disabled>';
 			$html .= '<input type="hidden" class="update_" name="allergen_icon_hidden" value="' . ((!empty($this->_allergen)) ? $this->_allergen['attachment_name'] : "") . '" disabled>';
-			$html .= '<span>Set image</span>';
+			$html .= '<span>'.__('Set image', 'allergens-dietary-ictoria') .'</span>';
 			$html .= '</label></div><br></fieldset>';
 			$html .= '</div></fieldset">';
 		} else {
