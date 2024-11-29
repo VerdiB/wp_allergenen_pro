36ca80786cf4111f47e12e532275ecffaee706c3
Verdi-B <151911787+Verdi-B@users.noreply.github.com>
2024-11-29 10:09:39 +0100

    hopefully fixed the bug in the license form (#83)

--- allergens-dietary-ictoria/php/forms/allergen_form_license.php
+++ allergens-dietary-ictoria/php/forms/allergen_form_license.php
@@ -40,7 +40,7 @@ class Allergens_Dietary_Ictoria_License_Form implements I_Allergens_Dietary_Icto
 		// TODO: Getting license key that is in use by site if it exists
 		$html  = '<fieldset>
 		<label for="license_key">' . __( 'License key', 'allergens-dietary-ictoria' ) . '</label><br>
-		<input type="text" name="license_key" id="license_key" value="">' . _( 'License key', 'allergens-dietary-ictoria' ) . '<br><br>
+		<input type="text" name="license_key" id="license_key" value=""><br><br>
 		<input type="submit" class="button button-primary" id="submitButton" name="submit" value="' . __( 'Verify license key', 'allergens-dietary-ictoria' ) . '">';
 		$html .= '</fieldset>';
 
